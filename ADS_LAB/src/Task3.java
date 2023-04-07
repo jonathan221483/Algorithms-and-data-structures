2d18c5d6b1585c6925628f566775e1312e8cda84
jonathan221483 <captainberkut@gmail.com>
2023-04-07 17:49:54 +0600

    task3

--- ADS_LAB/src/Task3.java
+++ ADS_LAB/src/Task3.java
@@ -13,19 +13,19 @@ public class Task3  {
 
     /**
      *
-     * @param array array with numbers
-     * @param i index of the array
-     * @return average of all numbers in array
+     * @param number the number that you need to check if its prime
+     * @param i counter that decreases and checks number for division
+     * @return returns 1 if number is prime and 0 if its composite
      */
-    public static int isPrime(int number, int i) {
+    public static int isPrime(int number, int i) { //function checks whether the number is prime or not
         if(i==1){ //stop the function if index reaches the final element of the array
             return 1;
         }
         else //recursive case
         {
             int prime = 1;
-            prime *= isPrime(number,i-1);
-            if(number % i == 0)
+            prime *= isPrime(number,i-1); //multiplication works just like AND operator in boolean logic
+            if(number % i == 0) //check for the division remainder
             {
                 return 0;
             }
